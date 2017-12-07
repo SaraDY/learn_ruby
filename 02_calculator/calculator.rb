e628b15a4eb05ce500e2bfe9f7520de0bb491271
Sara Forler <saraforler@gmail.com>
2017-12-06 23:02:50 -0500

    Solution for 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add(x,y)
+  x + y
+end
+
+def subtract (x,y)
+  x - y
+end
+
+def sum (x)
+  x.reduce(0, :+)
+end
+
+def multiply(x,y)
+x * y
+end
+
+def power(x,y)
+  x**y
+end
+
+def factorial x
+  a = []
+  if x == 0
+    a.push(1)
+  end
+  while x >= 1
+    a.push(x)
+    x -= 1
+  end
+  a.reduce(:*)
+end
