a86c31297d97deab836b2363c1a3859a3bf0c06c
Sara Forler <saraforler@gmail.com>
2017-12-08 12:45:11 -0500

    Solution for 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo x
+x
+end
+
+def shout x
+  x.upcase
+end
+
+def repeat(x, y = 2)
+  ((x + " ") * y).chop
+  end
+
+def start_of_word(x, y)
+  x[0, y.to_i]
+end
+
+def first_word(string)
+  string.split.first
+end
+
+def titleize(x)
+  x = x.split.map! do |word|
+    if word != "and" && word != "over" && word != "the"
+      word.capitalize
+    else
+      word
+    end
+  end
+			x[0].capitalize!
+			x = x.join(" ")
+	end
