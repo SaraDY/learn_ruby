bff4a3cb7280fb84885406a137b8fd6120a21d87
Sara Forler <saraforler@gmail.com>
2018-01-26 11:27:40 -0500

    Solution for 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+
+temp = @title.split(" ")
+temp[0] = temp[0].capitalize
+
+temp.map do |word|
+  if ['and', 'in', 'the', 'of', 'a', 'an'].include?(word)
+    word
+  else
+    word.capitalize
+  end
+end.join(" ")
+
+end
 end
